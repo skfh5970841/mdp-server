e3d5c6003d60f5df4ea46df36d50b4b949e384ce
ryougi <dkfk5970842@naver.com>
2019-07-19 12:44:44 +0900

    added

--- router/main.js
+++ router/main.js
@@ -7,37 +7,37 @@ module.exports = function(app, fs)
 
     connection.connect();
 // 일정한 시간마다 의미없는 쿼리문을 보내서 연결을 유지시킨다.
-    setInterval(()=> {
-        connection.query('SELECT 1');
-    }, 5000);
-    app.get('/', (req, res)=>{
-        if(req.session.user){
-            res.render('user.ejs', {
-                Id : req.session.user.Id
-            });
-        }else {
-            res.redirect('/login');
-        }
-    });
-    app.get('/login', (req, res)=>{
-        res.render('login.ejs');
-    });
+setInterval(()=> {
+    connection.query('SELECT 1');
+}, 5000);
+app.get('/', (req, res)=>{
+    if(req.session.user){
+        res.render('user.ejs', {
+            Id : req.session.user.Id
+        });
+    }else {
+        res.redirect('/login');
+    }
+});
+app.get('/login', (req, res)=>{
+    res.render('login.ejs');
+});
 
-    app.post('/login', (req, res)=>{
-        var id = req.body.username;
-        var pw = req.body.password;
-        var session = req.session;
+app.post('/login', (req, res)=>{
+    var id = req.body.username;
+    var pw = req.body.password;
+    var session = req.session;
 
-        connection.query('SELECT * FROM student WHERE Stuid = ?', [id],
-            (error, results, fields) =>{
-                if (error) 
-                {
-                    console.log(error);
-                    res.send({
-                        "code": 400,
-                        "failed": "error ocurred"
-                    })
-                } else {
+    connection.query('SELECT * FROM student WHERE Stuid = ?', [id],
+        (error, results, fields) =>{
+            if (error) 
+            {
+                console.log(error);
+                res.send({
+                    "code": 400,
+                    "failed": "error ocurred"
+                })
+            } else {
                 if(results.length > 0) {
                     //console.log(results[0].StuPw);
 
@@ -62,7 +62,7 @@ module.exports = function(app, fs)
             }
         }    
     })
-    });
+});
 
     //제작중
     app.post('/process/nfc', (req, res)=>{
@@ -117,5 +117,57 @@ module.exports = function(app, fs)
     app.get('/admin', (req, res)=>{
         res.render('admin.ejs')
     })
+
+    app.post('/adduser'(req, res)=>{
+        var name = req.body.username;
+        var id = req.body.Id;
+        var pw = req.body.password;
+        var cpw = req.body.confirm_password;
+        var email = reeq.body.email;
+
+        if(name||id||pw||cpw||email){
+            alert('모든 정보가 입력되었습니다.');
+
+            connection.query('SELECT * FROM student WHERE Stuid = ?', [id],
+                (error, results, fields) =>{
+                    if (error) 
+                    {
+                        console.log(error);
+                        res.send({
+                            "code": 400,
+                            "failed": "error ocurred"
+                        })
+                    } else {
+                        if(results.length > 0) {
+                    //console.log(results[0].StuPw);
+
+                    if(results[0].StuPw == pw) {
+                     session.user = {
+                        "Id" : results[0].StuId,
+                        "age" : 25,
+                    }                 
+                    res.redirect('/');
+
+                } else {
+                    res.send({
+                        "code": 204,
+                        "success": "id and password does not match"
+                    });
+                }
+            } else {
+                res.send({
+                    "code":204,
+                    "success": "Id does not exists"
+                });
+            }
+        }    
+        
+    });
+        }
+        else{
+            alert('입력하지 않는 부분이 있습니다. 모두 입력하여 주세요');
+            res.redirect('/admin');
+        }
+    });
     
 }
