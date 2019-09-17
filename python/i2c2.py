63fdca121299586b664512c2029fcfaddd0b2170
ryougi <dkfk5970842@naver.com>
2019-09-17 11:08:22 +0900

    commited

--- python/i2c2.py
+++ python/i2c2.py
@@ -5,7 +5,7 @@ import RPi.GPIO as GPIO
 import requests
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(1, GPIO.OUT)
+GPIO.setup(21, GPIO.OUT)
 
 url = "http://localhost:8888/process/nfc"
 pn532 = Pn532_i2c()
@@ -18,8 +18,8 @@ response = requests.post(url, data=json.dumps(card_data))
 print(response.text)
 
 if response.text == 'OK' : 
-    GPIO.output(1, 1)
+    GPIO.output(21, 1)
 else
-    GPIO.output(1, 0)
+    GPIO.output(21, 0)
 
 
