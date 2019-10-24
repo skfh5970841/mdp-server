990e5cf7a478b06994dbe7c9ca3bd900bda001a3
ryougi <dkfk5970842@naver.com>
2019-10-24 19:15:37 +0900

    commited

--- server.js
+++ server.js
@@ -12,6 +12,14 @@ var MYSQLStore = require('express-mysql-session')(session);
 var sessionStore = new MYSQLStore(config);
 var fs = require('fs');
 
+var
+    exists = fs.exists || path.exists,
+    tmpDir = os.tmpDir || _getTMPDir,
+    tmpDir = os.tmpdir || os.tmpDir || _getTMPDir,
+    _TMP = tmpDir(),
+    randomChars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz",
+    randomCharsLength = randomChars.length;
+
 app.use(bodyParser.json());
 app.use(session({
     secret: 'zenbusine!!!!!',
