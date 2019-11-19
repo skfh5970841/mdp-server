c96587f2fb066f116c40d7e7e206a1f5c8fe8d0e
ryougi <dkfk5970842@naver.com>
2019-11-19 14:47:23 +0900

    Comited

--- python/munhak.py
+++ python/munhak.py
@@ -1,18 +1,33 @@
-# import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 import requests
 import json
 import time
-# GPIO.setmode(GPIO.BCM)
-# GPIO.setup(4, GPIO.OUT)
-# GPIO.setup(5, GPIO.OUT)
-# GPIO.setup(6, GPIO.OUT)
-# GPIO.setup(10, GPIO.OUT)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(4, GPIO.OUT)
+GPIO.setup(5, GPIO.OUT)
+GPIO.setup(6, GPIO.OUT)
+GPIO.setup(10, GPIO.OUT)
 url = "http://localhost:8888/munhak"
 
 while True : 
     response = requests.post(url)
     print(response.text)
-    
+    if response.text[0] == 1 : 
+        GPIO.output(4, 1)
+    else
+        GPIO.output(4, 0)
+    if response.text[1] == 1 :
+        GPIO.output(5, 1)
+    else
+        GPIO.output(5, 0)
+    if response.text[2] == 1 :
+        GPIO.output(6, 1)
+    else
+        GPIO.output(6, 0)
+    if response.text[3] == 1 :
+        GPIO.output(10, 1)
+    else
+        GPIO.output(10, 0)
 
 
 
