0d91c058d093b299b8b96e4c2cb56f6a0ac1bc00
ryougi <dkfk5970842@naver.com>
2019-11-20 15:48:35 +0900

    commtied

--- python/munhak.py
+++ python/munhak.py
@@ -6,28 +6,28 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(4, GPIO.OUT)
 GPIO.setup(5, GPIO.OUT)
 GPIO.setup(6, GPIO.OUT)
-GPIO.setup(10, GPIO.OUT)
+GPIO.setup(11, GPIO.OUT)
 url = "http://localhost:8888/munhak"
 #url = "http://zenbusine.herokuapp.com/munhak"
 
 while True : 
     response = requests.post(url)
-    if response.text[0] == 1 : 
+    if response.text[1] == '1' : 
         GPIO.output(4, 1)
     else :
         GPIO.output(4, 0)
-    if response.text[1] == 1 :
+    if response.text[3] == '1' :
         GPIO.output(5, 1)
     else :
         GPIO.output(5, 0)
-    if response.text[2] == 1 :
+    if response.text[5] == '1' :
         GPIO.output(6, 1)
     else :
         GPIO.output(6, 0)
-    if response.text[3] == 1 :
-        GPIO.output(10, 1)
+    if response.text[7] == '1':
+        GPIO.output(11, 1)
     else :
-        GPIO.output(10, 0)
+        GPIO.output(11, 0)
     time.sleep(0.5)
 
 
